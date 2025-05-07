3994434a0472c92e747a4f364ab4833c3f5b266d
AdhamIsoqov <adham29.09.2004@gmail.com>
2025-05-07 18:16:41 +0500

    Dizany yangilandi

--- Sayohatchi.uz/NewCustumers.Designer.cs
+++ Sayohatchi.uz/NewCustumers.Designer.cs
@@ -31,18 +31,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.FISHtxt = new System.Windows.Forms.TextBox();
-            this.Agetxt = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.Phonetxt = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.Emailtxt = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.button1 = new System.Windows.Forms.Button();
+            this.AddCustumersBtn = new System.Windows.Forms.Button();
+            this.AgeTxtNumeric = new System.Windows.Forms.NumericUpDown();
+            this.Phonetxtmasked = new System.Windows.Forms.MaskedTextBox();
+            this.Emailtxt = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AgeTxtNumeric)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -58,7 +59,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(181, 170);
+            this.label2.Location = new System.Drawing.Point(181, 156);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 25);
             this.label2.TabIndex = 1;
@@ -66,63 +67,42 @@
             // 
             // FISHtxt
             // 
-            this.FISHtxt.Location = new System.Drawing.Point(361, 167);
+            this.FISHtxt.Location = new System.Drawing.Point(361, 153);
             this.FISHtxt.Name = "FISHtxt";
             this.FISHtxt.Size = new System.Drawing.Size(333, 30);
             this.FISHtxt.TabIndex = 2;
             // 
-            // Agetxt
-            // 
-            this.Agetxt.Location = new System.Drawing.Point(361, 203);
-            this.Agetxt.Name = "Agetxt";
-            this.Agetxt.Size = new System.Drawing.Size(333, 30);
-            this.Agetxt.TabIndex = 4;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(181, 206);
+            this.label3.Location = new System.Drawing.Point(181, 191);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(57, 25);
             this.label3.TabIndex = 3;
             this.label3.Text = "Yosh";
             // 
-            // Phonetxt
-            // 
-            this.Phonetxt.Location = new System.Drawing.Point(361, 239);
-            this.Phonetxt.Name = "Phonetxt";
-            this.Phonetxt.Size = new System.Drawing.Size(333, 30);
-            this.Phonetxt.TabIndex = 6;
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(181, 242);
+            this.label4.Location = new System.Drawing.Point(181, 228);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(138, 25);
             this.label4.TabIndex = 5;
             this.label4.Text = "Telefon raqam";
             // 
-            // Emailtxt
-            // 
-            this.Emailtxt.Location = new System.Drawing.Point(361, 275);
-            this.Emailtxt.Name = "Emailtxt";
-            this.Emailtxt.Size = new System.Drawing.Size(333, 30);
-            this.Emailtxt.TabIndex = 8;
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(181, 278);
+            this.label5.Location = new System.Drawing.Point(181, 264);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(93, 25);
             this.label5.TabIndex = 7;
-            this.label5.Text = "E-Pachta";
+            this.label5.Text = "E-Pochta";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(181, 314);
+            this.label6.Location = new System.Drawing.Point(181, 298);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(52, 25);
             this.label6.TabIndex = 9;
@@ -132,7 +112,7 @@
             // 
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(361, 314);
+            this.groupBox1.Location = new System.Drawing.Point(361, 298);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(333, 25);
             this.groupBox1.TabIndex = 10;
@@ -160,31 +140,58 @@
             this.radioButton1.Text = "Erkak";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // AddCustumersBtn
             // 
-            this.button1.BackColor = System.Drawing.Color.DarkGreen;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(367, 369);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 36);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Qo\'shish";
-            this.button1.UseVisualStyleBackColor = false;
+            this.AddCustumersBtn.BackColor = System.Drawing.Color.DarkGreen;
+            this.AddCustumersBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddCustumersBtn.ForeColor = System.Drawing.Color.White;
+            this.AddCustumersBtn.Location = new System.Drawing.Point(367, 369);
+            this.AddCustumersBtn.Name = "AddCustumersBtn";
+            this.AddCustumersBtn.Size = new System.Drawing.Size(134, 36);
+            this.AddCustumersBtn.TabIndex = 11;
+            this.AddCustumersBtn.Text = "Qo\'shish";
+            this.AddCustumersBtn.UseVisualStyleBackColor = false;
+            // 
+            // AgeTxtNumeric
+            // 
+            this.AgeTxtNumeric.Location = new System.Drawing.Point(361, 189);
+            this.AgeTxtNumeric.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.AgeTxtNumeric.Name = "AgeTxtNumeric";
+            this.AgeTxtNumeric.Size = new System.Drawing.Size(333, 30);
+            this.AgeTxtNumeric.TabIndex = 12;
+            // 
+            // Phonetxtmasked
+            // 
+            this.Phonetxtmasked.Location = new System.Drawing.Point(361, 225);
+            this.Phonetxtmasked.Mask = "(00) 000-00-00";
+            this.Phonetxtmasked.Name = "Phonetxtmasked";
+            this.Phonetxtmasked.Size = new System.Drawing.Size(333, 30);
+            this.Phonetxtmasked.TabIndex = 13;
+            // 
+            // Emailtxt
+            // 
+            this.Emailtxt.Location = new System.Drawing.Point(361, 261);
+            this.Emailtxt.Name = "Emailtxt";
+            this.Emailtxt.Size = new System.Drawing.Size(333, 30);
+            this.Emailtxt.TabIndex = 14;
             // 
             // NewCustumers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(925, 581);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Emailtxt);
+            this.Controls.Add(this.Phonetxtmasked);
+            this.Controls.Add(this.AgeTxtNumeric);
+            this.Controls.Add(this.AddCustumersBtn);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.Emailtxt);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.Phonetxt);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.Agetxt);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.FISHtxt);
             this.Controls.Add(this.label2);
@@ -196,6 +203,7 @@
             this.Text = "NewCustumers";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AgeTxtNumeric)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -206,16 +214,16 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox FISHtxt;
-        private System.Windows.Forms.TextBox Agetxt;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox Phonetxt;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox Emailtxt;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button AddCustumersBtn;
+        private System.Windows.Forms.NumericUpDown AgeTxtNumeric;
+        private System.Windows.Forms.MaskedTextBox Phonetxtmasked;
+        private System.Windows.Forms.TextBox Emailtxt;
     }
 }
