d8b07554401a36b13e8a0d4d3e0a29d04d09b609
AdhamIsoqov <adham29.09.2004@gmail.com>
2025-05-09 02:46:09 +0500

    UpdateCustumers.cs yakuniga yetdi

--- Sayohatchi.uz/ViewCustumers.Designer.cs
+++ Sayohatchi.uz/ViewCustumers.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.ViewCustumersDataGridView = new System.Windows.Forms.DataGridView();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.DeleteViewCustumersDataGridView = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TravelersFISHTxt = new System.Windows.Forms.Label();
+            this.DeleteTravelerBtn = new System.Windows.Forms.Button();
+            this.TravelerIDColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TravelerFISHColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TravelerEmailColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TravelerPhoneNumberColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TravelerGenderColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.ViewCustumersDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,66 +55,94 @@
             // 
             this.ViewCustumersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.ViewCustumersDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column2,
-            this.Column3,
-            this.Column4,
-            this.Column5,
-            this.Column6,
-            this.DeleteViewCustumersDataGridView});
+            this.TravelerIDColumn,
+            this.TravelerFISHColumn,
+            this.TravelerEmailColumn,
+            this.TravelerPhoneNumberColumn,
+            this.TravelerGenderColumn});
             this.ViewCustumersDataGridView.Location = new System.Drawing.Point(32, 84);
             this.ViewCustumersDataGridView.Name = "ViewCustumersDataGridView";
             this.ViewCustumersDataGridView.RowHeadersWidth = 51;
             this.ViewCustumersDataGridView.RowTemplate.Height = 24;
-            this.ViewCustumersDataGridView.Size = new System.Drawing.Size(861, 431);
+            this.ViewCustumersDataGridView.Size = new System.Drawing.Size(861, 401);
             this.ViewCustumersDataGridView.TabIndex = 15;
+            this.ViewCustumersDataGridView.SelectionChanged += new System.EventHandler(this.ViewCustumersDataGridView_SelectionChanged);
             // 
-            // Column2
+            // label2
             // 
-            this.Column2.HeaderText = "ID";
-            this.Column2.MinimumWidth = 6;
-            this.Column2.Name = "Column2";
-            this.Column2.Width = 50;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 501);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 25);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Sayohatchi ";
             // 
-            // Column3
+            // TravelersFISHTxt
             // 
-            this.Column3.HeaderText = "FISH";
-            this.Column3.MinimumWidth = 6;
-            this.Column3.Name = "Column3";
-            this.Column3.Width = 300;
+            this.TravelersFISHTxt.AutoSize = true;
+            this.TravelersFISHTxt.Location = new System.Drawing.Point(153, 501);
+            this.TravelersFISHTxt.Name = "TravelersFISHTxt";
+            this.TravelersFISHTxt.Size = new System.Drawing.Size(133, 25);
+            this.TravelersFISHTxt.TabIndex = 17;
+            this.TravelersFISHTxt.Text = "Tanlanmagan";
             // 
-            // Column4
+            // DeleteTravelerBtn
             // 
-            this.Column4.HeaderText = "Yosh";
-            this.Column4.MinimumWidth = 6;
-            this.Column4.Name = "Column4";
-            this.Column4.Width = 75;
+            this.DeleteTravelerBtn.BackColor = System.Drawing.Color.DarkRed;
+            this.DeleteTravelerBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeleteTravelerBtn.ForeColor = System.Drawing.Color.White;
+            this.DeleteTravelerBtn.Location = new System.Drawing.Point(768, 495);
+            this.DeleteTravelerBtn.Name = "DeleteTravelerBtn";
+            this.DeleteTravelerBtn.Size = new System.Drawing.Size(125, 36);
+            this.DeleteTravelerBtn.TabIndex = 39;
+            this.DeleteTravelerBtn.Text = "O\'chirish";
+            this.DeleteTravelerBtn.UseVisualStyleBackColor = false;
+            this.DeleteTravelerBtn.Click += new System.EventHandler(this.DeleteTravelerBtn_Click);
             // 
-            // Column5
+            // TravelerIDColumn
             // 
-            this.Column5.HeaderText = "Telefon raqam";
-            this.Column5.MinimumWidth = 6;
-            this.Column5.Name = "Column5";
-            this.Column5.Width = 200;
+            this.TravelerIDColumn.HeaderText = "ID";
+            this.TravelerIDColumn.MinimumWidth = 6;
+            this.TravelerIDColumn.Name = "TravelerIDColumn";
+            this.TravelerIDColumn.Visible = false;
+            this.TravelerIDColumn.Width = 125;
             // 
-            // Column6
+            // TravelerFISHColumn
             // 
-            this.Column6.HeaderText = "E-Pochta";
-            this.Column6.MinimumWidth = 6;
-            this.Column6.Name = "Column6";
-            this.Column6.Width = 250;
+            this.TravelerFISHColumn.HeaderText = "F.I.SH";
+            this.TravelerFISHColumn.MinimumWidth = 6;
+            this.TravelerFISHColumn.Name = "TravelerFISHColumn";
+            this.TravelerFISHColumn.Width = 300;
             // 
-            // DeleteViewCustumersDataGridView
+            // TravelerEmailColumn
             // 
-            this.DeleteViewCustumersDataGridView.HeaderText = "O\'chirish";
-            this.DeleteViewCustumersDataGridView.MinimumWidth = 6;
-            this.DeleteViewCustumersDataGridView.Name = "DeleteViewCustumersDataGridView";
-            this.DeleteViewCustumersDataGridView.Width = 125;
+            this.TravelerEmailColumn.HeaderText = "E-Pochta";
+            this.TravelerEmailColumn.MinimumWidth = 6;
+            this.TravelerEmailColumn.Name = "TravelerEmailColumn";
+            this.TravelerEmailColumn.Width = 208;
+            // 
+            // TravelerPhoneNumberColumn
+            // 
+            this.TravelerPhoneNumberColumn.HeaderText = "Telefon raqam";
+            this.TravelerPhoneNumberColumn.MinimumWidth = 6;
+            this.TravelerPhoneNumberColumn.Name = "TravelerPhoneNumberColumn";
+            this.TravelerPhoneNumberColumn.Width = 175;
+            // 
+            // TravelerGenderColumn
+            // 
+            this.TravelerGenderColumn.HeaderText = "Jinsi";
+            this.TravelerGenderColumn.MinimumWidth = 6;
+            this.TravelerGenderColumn.Name = "TravelerGenderColumn";
+            this.TravelerGenderColumn.Width = 125;
             // 
             // ViewCustumers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(925, 581);
+            this.Controls.Add(this.DeleteTravelerBtn);
+            this.Controls.Add(this.TravelersFISHTxt);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.ViewCustumersDataGridView);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -131,11 +161,13 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView ViewCustumersDataGridView;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
-        private System.Windows.Forms.DataGridViewTextBoxColumn DeleteViewCustumersDataGridView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label TravelersFISHTxt;
+        private System.Windows.Forms.Button DeleteTravelerBtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TravelerIDColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TravelerFISHColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TravelerEmailColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TravelerPhoneNumberColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TravelerGenderColumn;
     }
 }
--- Sayohatchi.uz/ViewCustumers.cs
+++ Sayohatchi.uz/ViewCustumers.cs
@@ -27,11 +27,110 @@ namespace Sayohatchi.uz
                 {
                     connection.Open();
                     MessageBox.Show("Ulanish muvaffaqiyatli!");
+                    LoadCustomers();
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Ulanishda xato: " + ex.Message);
+            }
+        }
+        private void LoadCustomers()
+        {
+            try
+            {
+                ViewCustumersDataGridView.Rows.Clear();
+
+                using (SqlConnection connection = new SqlConnection(conn))
+                {
+                    connection.Open();
+                    string selectQuery = "SELECT id, full_name, email, phone_number, gender FROM travelers"; 
+                    using (SqlCommand command = new SqlCommand(selectQuery, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                int travelerId = reader.GetInt32(0);
+                                string travelerFISH = reader.GetString(1);  
+                                string travelerEmail = reader.GetString(2); 
+                                string travelerPhoneNumber = reader.GetString(3);  
+                                string travelerGender = reader.GetString(4); 
+                                int rowIndex = ViewCustumersDataGridView.Rows.Add(); 
+                                DataGridViewRow row = ViewCustumersDataGridView.Rows[rowIndex];
+                                row.Cells["TravelerIDColumn"].Value = travelerId;
+                                row.Cells["TravelerFISHColumn"].Value = travelerFISH; 
+                                row.Cells["TravelerEmailColumn"].Value = travelerEmail;
+                                row.Cells["TravelerPhoneNumberColumn"].Value = travelerPhoneNumber; 
+                                row.Cells["TravelerGenderColumn"].Value = travelerGender;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Xato: " + ex.Message);
+            }
+        }
+        private void ViewCustumersDataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            if (ViewCustumersDataGridView.SelectedRows.Count > 0)
+            {
+                var selectedRow = ViewCustumersDataGridView.SelectedRows[0];
+                int travelerId = Convert.ToInt32(selectedRow.Cells["TravelerIDColumn"].Value);
+                string fullName = selectedRow.Cells["TravelerFISHColumn"].Value.ToString();
+                TravelersFISHTxt.Text = fullName;
+                DeleteTravelerBtn.Enabled = true;
+                currentTravelerId = travelerId;
+            }
+            else
+            {
+                
+                DeleteTravelerBtn.Enabled = false;
+            }
+        }
+        private int currentTravelerId = -1; 
+        private void DeleteTravelerBtn_Click(object sender, EventArgs e)
+        {
+            if (currentTravelerId != -1)
+            {
+                DialogResult result = MessageBox.Show("Sayohatchi ma'lumotlarini o'chirmoqchimisiz?", "Tasdiqlash", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        using (SqlConnection connection = new SqlConnection(conn))
+                        {
+                            connection.Open();
+                            string deleteQuery = "DELETE FROM travelers WHERE id = @id";
+                            using (SqlCommand command = new SqlCommand(deleteQuery, connection))
+                            {
+                                command.Parameters.AddWithValue("@id", currentTravelerId); 
+                                int rowsAffected = command.ExecuteNonQuery();
+
+                                if (rowsAffected > 0)
+                                {
+                                    MessageBox.Show("Sayohatchi muvaffaqiyatli o'chirildi.", "O'chirish", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                    LoadCustomers(); 
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Sayohatchi o'chirilmadi. Iltimos, yana bir bor tekshiring.", "Xato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Xato: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Iltimos, o'chirmoqchi bo'lgan sayohatchini tanlang.", "Ogohlantirish", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
     }
