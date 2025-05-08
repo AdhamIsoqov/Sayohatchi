f813d520d42bd148a2d8d1bce904a68a36ceec2d
AdhamIsoqov <adham29.09.2004@gmail.com>
2025-05-08 15:00:10 +0500

    xatolik ketdi

--- Sayohatchi.uz/NewCustumers.cs
+++ Sayohatchi.uz/NewCustumers.cs
@@ -19,8 +19,6 @@ namespace Sayohatchi.uz
             InitializeComponent();
         }
         string conn = ConfigurationManager.ConnectionStrings["DataBaseConn"].ConnectionString;
-<<<<<<< HEAD
-=======
         private void AddCustumersBtn_Click(object sender, EventArgs e)
         {
             string fullName = FISHtxt.Text;
@@ -28,12 +26,8 @@ namespace Sayohatchi.uz
             string phoneNumber = Phonetxtmasked.Text;
             string email = Emailtxt.Text;
             string gender = radioButton1.Checked ? "Erkak" : "Ayol";
-
-            // Jadvalga qo'shish uchun SQL so'rovi
             string insertQuery = "INSERT INTO [dbo].[travelers] (full_name, email, phone_number, gender) " +
                                  "VALUES (@fullName, @email, @phoneNumber, @gender)";
-
-            // Ulanish va ma'lumotlarni qo'shish
             try
             {
                 string connectionString = conn;
@@ -63,8 +57,6 @@ namespace Sayohatchi.uz
                 MessageBox.Show("Xato yuz berdi: " + ex.Message);
             }
         }
-
->>>>>>> efc2f3ea402597be99cd9d54ba870868d44bfbcc
         private void NewCustumers_Load(object sender, EventArgs e)
         {
             try
@@ -78,45 +70,6 @@ namespace Sayohatchi.uz
             catch (Exception ex)
             {
                 MessageBox.Show("Ulanishda xato: " + ex.Message);
-            }
-        }
-        private void AddCustumersBtn_Click(object sender, EventArgs e)
-        {
-            string fullName = FISHtxt.Text;
-            int age = (int)AgeTxtNumeric.Value;
-            string phoneNumber = Phonetxtmasked.Text;
-            string email = Emailtxt.Text;
-            string gender = radioButton1.Checked ? "Erkak" : "Ayol";
-            string insertQuery = "INSERT INTO [dbo].[travelers] (full_name, email, phone_number, gender) " +
-                                 "VALUES (@fullName, @email, @phoneNumber, @gender)";
-            
-            try
-            {
-                string connectionString = conn;
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    SqlCommand command = new SqlCommand(insertQuery, connection);
-                    command.Parameters.AddWithValue("@fullName", fullName);
-                    command.Parameters.AddWithValue("@email", email);
-                    command.Parameters.AddWithValue("@phoneNumber", phoneNumber);
-                    command.Parameters.AddWithValue("@gender", gender);
-
-                    connection.Open();
-                    int rowsAffected = command.ExecuteNonQuery();
-
-                    if (rowsAffected > 0)
-                    {
-                        MessageBox.Show("Mijoz muvaffaqiyatli qo'shildi!");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Mijoz qo'shilishda xato yuz berdi.");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Xato yuz berdi: " + ex.Message);
             }
         }
     }
--- Sayohatchi.uz/ViewCustumers.Designer.cs
+++ Sayohatchi.uz/ViewCustumers.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ViewCustumersDataGridView = new System.Windows.Forms.DataGridView();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.DeleteViewCustumersDataGridView = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.ViewCustumersDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -49,22 +49,22 @@
             this.label1.TabIndex = 14;
             this.label1.Text = "Sayohatchilarni ko\'rish";
             // 
-            // dataGridView1
+            // ViewCustumersDataGridView
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ViewCustumersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ViewCustumersDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column2,
             this.Column3,
             this.Column4,
             this.Column5,
             this.Column6,
-            this.Column1});
-            this.dataGridView1.Location = new System.Drawing.Point(32, 84);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(861, 431);
-            this.dataGridView1.TabIndex = 15;
+            this.DeleteViewCustumersDataGridView});
+            this.ViewCustumersDataGridView.Location = new System.Drawing.Point(32, 84);
+            this.ViewCustumersDataGridView.Name = "ViewCustumersDataGridView";
+            this.ViewCustumersDataGridView.RowHeadersWidth = 51;
+            this.ViewCustumersDataGridView.RowTemplate.Height = 24;
+            this.ViewCustumersDataGridView.Size = new System.Drawing.Size(861, 431);
+            this.ViewCustumersDataGridView.TabIndex = 15;
             // 
             // Column2
             // 
@@ -101,19 +101,19 @@
             this.Column6.Name = "Column6";
             this.Column6.Width = 250;
             // 
-            // Column1
+            // DeleteViewCustumersDataGridView
             // 
-            this.Column1.HeaderText = "O\'chirish";
-            this.Column1.MinimumWidth = 6;
-            this.Column1.Name = "Column1";
-            this.Column1.Width = 75;
+            this.DeleteViewCustumersDataGridView.HeaderText = "O\'chirish";
+            this.DeleteViewCustumersDataGridView.MinimumWidth = 6;
+            this.DeleteViewCustumersDataGridView.Name = "DeleteViewCustumersDataGridView";
+            this.DeleteViewCustumersDataGridView.Width = 125;
             // 
             // ViewCustumers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(925, 581);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ViewCustumersDataGridView);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -121,7 +121,7 @@
             this.Name = "ViewCustumers";
             this.Text = "ViewCustumers";
             this.Load += new System.EventHandler(this.ViewCustumers_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ViewCustumersDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,12 +130,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView ViewCustumersDataGridView;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DeleteViewCustumersDataGridView;
     }
 }
